cc27e260dd93d16a0451952229cc25d503dd74fb
Fausto Carrera <fcarrera@gmail.com>
2015-11-24 02:42:49 +0000

    Added basic posts list template and static data.

---
+++ client/templates/posts_list.js
@@ -0,0 +1,20 @@
+//Create initial data
+var postsData = [
+  {
+    title: 'Introducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  },
+  {
+    title: 'Meteor',
+    url: 'http://meteor.com'
+  },
+  {
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com'
+  }
+];
+
+//Data helpers for template
+Template.tplPostsList.helpers({
+  posts: postsData
+});
